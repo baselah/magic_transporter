41e8775153ba90a6431d5512729ef63a0245585e
basel alhawari <basel.ah.991@gmail.com>
2024-01-21 01:46:22 +0300

    fix mongo

--- app.ts
+++ app.ts
@@ -1,12 +1,11 @@
 import dotenv from "dotenv";
 import express from "express";
 import bodyParser from 'body-parser';
-import mongoose from './config/database';
+import './config/database';
 import router from "./src/User/router";
 const app = express();
 dotenv.config();
 app.use(bodyParser.json());
-require('dotenv').config();
 
 app.use("/league/api/user", router);
 app.listen(process.env.PORT, function () {
--- config/database.ts
+++ config/database.ts
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 
 mongoose.connect(
-  "mongodb+srv://baselah991:<password>@cluster0.ank1pxn.mongodb.net/?retryWrites=true&w=majority",
+  "mongodb+srv://root:root@cluster0.ank1pxn.mongodb.net/?retryWrites=true&w=majority",
   {}
 );
 
 const db = mongoose.connection;
+
+console.log('sadddddd');
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", () => {
   console.log("Connected to MongoDB");
 });
 
-export default mongoose;
